ca31b4c1564d8f66fa92348647bb9adc1a75bfdf
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-03-26 21:21:23 -0700

    arrow fx

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Hello Node!");
+// .slice() returns everythign from first index as 2nd arg
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+// no parentheses around profileDataArr parameter
+const printProfileData = profileDataArr => {
+  for (let i = 0; i < profileDataArr.length; i++) {
+    console.log(profileDataArr[i]);
+  }
+};
+
+console.log("================");
+
+//   is the same as....
+// forEach executes function on each element of array using value
+//  of element at that iteration as argument
+profileDataArr.forEach(profileItem => console.log(profileItem));
+
+printProfileData(profileDataArgs);
