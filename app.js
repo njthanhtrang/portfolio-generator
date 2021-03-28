3715890b6d3dc1ac8e96cae22887881700cb4640
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-03-27 17:51:13 -0700

    modularized with src file

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
-// .slice() returns everythign from first index as 2nd arg
+// require used in destination files we want to receive exported fx
+const fs = require("fs");
+const generatePage = require("./src/page-template");
+
+// .slice() returns everything from first index as 2nd arg
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+// assignment destructuring assigns elements of array to variable names in single exp
+const [name, github] = profileDataArgs;
 
-// no parentheses around profileDataArr parameter
-const printProfileData = profileDataArr => {
-  for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-  }
-};
 
-console.log("================");
+fs.writeFile("./index.html", generatePage(name, github), err => {
+    // stops execution of code if error exists
+    if (err) throw new Error(err);
 
-//   is the same as....
-// forEach executes function on each element of array using value
-//  of element at that iteration as argument
-profileDataArr.forEach(profileItem => console.log(profileItem));
+    console.log("Portfolio complete! Check out index.html to see the output!");
+});
 
-printProfileData(profileDataArgs);
+// // no parentheses around profileDataArr parameter
+// const printProfileData = (profileDataArr) => {
+//   for (let i = 0; i < profileDataArr.length; i++) {
+//     console.log(profileDataArr[i]);
+//   }
+//   console.log("================");
+
+//   //   is the same as....
+//   // forEach executes function on each element of array using value
+//   //  of element at that iteration as argument
+//   profileDataArr.forEach((profileItem) => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
