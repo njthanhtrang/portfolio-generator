8c652014cb1db36a72d07ae443779b492859b171
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-03-29 13:03:41 -0700

    added inquirer input

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-// require used in destination files we want to receive exported fx
-const fs = require("fs");
-const generatePage = require("./src/page-template");
+const inquirer = require("inquirer");
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What's your name?"
+        }
+    ])
+    .then(answers => console.log(answers));
+    
 
-// .slice() returns everything from first index as 2nd arg
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// assignment destructuring assigns elements of array to variable names in single exp
-const [name, github] = profileDataArgs;
 
+// require used in destination files(app.js) we want to receive exported fx
+// const fs = require("fs");
+// const generatePage = require("./src/page-template");
 
-fs.writeFile("./index.html", generatePage(name, github), err => {
-    // stops execution of code if error exists
-    if (err) throw new Error(err);
+// const pageHTML = generatePage(name, github);
 
-    console.log("Portfolio complete! Check out index.html to see the output!");
-});
+// fs.writeFile("./index.html", pageHTML, err => {
+//     // stops execution of code if error exists
+//     if (err) throw new Error(err);
+
+//     console.log("Portfolio complete! Check out index.html to see the output!");
+// });
+
 
 // // no parentheses around profileDataArr parameter
 // const printProfileData = (profileDataArr) => {
