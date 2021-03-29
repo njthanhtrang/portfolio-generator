da887b0c9ce8e65c30bccdc133fa59d562544090
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-03-29 16:03:03 -0700

    validated inputs

--- app.js
+++ app.js
@@ -5,14 +5,28 @@ const promptUser = () => {
     {
       type: "input",
       name: "name",
-      message: "What's your name?",
+      message: "What's your name? (Required)",
+      validate: nameInput => {
+          if (nameInput) {
+              return true;
+          } else {
+              console.log("Please enter your name!");
+              return false;
+          }
+      }
     },
     {
       type: "input",
       name: "github",
-      message: "Enter your GitHub Username",
+      message: "Enter your GitHub Username (Required)",
+      validate: usernameInput => {
+        if (usernameInput) {
+            return true;
+        } else {
+            console.log("Please enter your GitHub username!");
+            return false;
+        }
     },
-    {
       type: "input",
       name: "about",
       message: "Provide some information about yourself:",
@@ -37,14 +51,26 @@ const promptProject = (portfolioData) => {
       {
         type: "input",
         name: "name",
-        message: "What is the name of your project?",
+        message: "What is the name of your project? (Required)",
+        validate: projectNameInput => {
+            if (projectNameInput) {
+                return true;
+            } else {
+                console.log("Please enter your project name!");
+                return false;
+            }
       },
-      {
         type: "input",
         name: "description",
         message: "Provide a description of the project (Required)",
+        validate: projectDescriptionInput => {
+            if (projectDescriptionInput) {
+                return true;
+            } else {
+                console.log("Please enter your project description!");
+                return false;
+            }
       },
-      {
         type: "checkbox",
         name: "languages",
         message: "What did you build this project with? (Check all that apply)",
@@ -62,8 +88,14 @@ const promptProject = (portfolioData) => {
         type: "input",
         name: "link",
         message: "Enter the GitHub link to your project. (Required)",
+        validate: githubLinkInput => {
+            if (githubLinkInput) {
+                return true;
+            } else {
+                console.log("Please enter your GitHub link!");
+                return false;
+            }
       },
-      {
         type: "confirm",
         name: "feature",
         message: "Would you like to feature this project?",
