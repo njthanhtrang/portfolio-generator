a15b26342eb24f2765760113990d9cec5f057474
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-03-29 14:42:44 -0700

    add multiple projects

--- app.js
+++ app.js
@@ -1,15 +1,97 @@
 const inquirer = require("inquirer");
-inquirer
-    .prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "What's your name?"
-        }
-    ])
-    .then(answers => console.log(answers));
-    
 
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What's your name?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username",
+    },
+    {
+      type: "input",
+      name: "about",
+      message: "Provide some information about yourself:",
+    },
+  ]);
+};
+
+const promptProject = (portfolioData) => {
+  console.log(`
+    =================
+    Add a New Project
+    =================
+    `);
+
+  // if there's no "projects" array property, create one
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of your project?",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Provide a description of the project (Required)",
+      },
+      {
+        type: "checkbox",
+        name: "languages",
+        message: "What did you build this project with? (Check all that apply)",
+        choices: [
+          "Javascript",
+          "HTML",
+          "CSS",
+          "ES6",
+          "jQuery",
+          "Bootstrap",
+          "Node",
+        ],
+      },
+      {
+        type: "input",
+        name: "link",
+        message: "Enter the GitHub link to your project. (Required)",
+      },
+      {
+        type: "confirm",
+        name: "feature",
+        message: "Would you like to feature this project?",
+        default: false,
+      },
+      {
+        type: "confirm",
+        name: "confirmAddProject",
+        message: "Would you like to enter another project?",
+        default: false,
+      },
+    ])
+    .then((projectData) => {
+      portfolioData.projects.push(projectData);
+      // if they wish to add more projects
+      if (projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+      } else {
+        return portfolioData;
+      }
+    });
+};
+
+promptUser()
+  .then(promptProject)
+  .then((portfolioData) => {
+    console.log(portfolioData);
+  });
 
 // require used in destination files(app.js) we want to receive exported fx
 // const fs = require("fs");
@@ -23,7 +105,6 @@ inquirer
 
 //     console.log("Portfolio complete! Check out index.html to see the output!");
 // });
-
 
 // // no parentheses around profileDataArr parameter
 // const printProfileData = (profileDataArr) => {
