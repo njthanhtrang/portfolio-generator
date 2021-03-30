2ac6e13e14b98825f5d6548283ababe5e30e258b
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-03-30 16:24:45 -0700

    corrections

--- src/page-template.js
+++ src/page-template.js
@@ -14,7 +14,7 @@ const generateAbout = (aboutText) => {
 };
 
 // create projects section
-const generateProjects = (projectsArr) => {
+const generateProjects = projectsArr => {
   return `
         <section class="my-3" id="portfolio">
             <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
@@ -29,7 +29,7 @@ const generateProjects = (projectsArr) => {
                     <h3 class="portfolio-item-title text-light">${name}</h3>
                     <h5 class="portfolio-languages">
                         Built With:
-                        ${languages.join(", ")}
+                        ${languages.map(language => language).join(", ")}
                     </h5>
                     <p>${description}</p>
                     <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
@@ -41,6 +41,7 @@ const generateProjects = (projectsArr) => {
         ${projectsArr
           .filter(({ feature }) => !feature)
           .map(({ name, description, languages, link }) => {
+              console.log(languages);
             return `
                 <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
                     <h3 class="portfolio-item-title text-light">${name}</h3>
@@ -59,9 +60,9 @@ const generateProjects = (projectsArr) => {
     `;
 };
 
+// export funxtion to generate entire page
 // module.exports can only be used ONCE in a file! if multiple functions or data, set to obj or arr
 // string interpolation: subbing text for a variable built into string
-// export funxtion to generate entire page
 // used in src file that has fx we want to make available
 module.exports = (templateData) => {
   // console.log(templateData);
